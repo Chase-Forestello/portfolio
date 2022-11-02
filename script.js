3be74dafb9d67c9fb30d05e1037e7feb0dac06fa
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-11-02 17:52:09 -0500

    redesigned entire layout

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
 (function () {
     "use strict";
-    // Back to top bottom that only shows once scrolling has begun with fixed position
-    const showOnPx = 100;
-    const backToTopButton = document.querySelector(".back-to-top")
+    let mybutton = document.getElementById("myBtn");
 
-    const scrollContainer = () => {
-        return document.documentElement || document.body;
-    };
+    window.onscroll = function() {scrollFunction()};
 
-    document.addEventListener("scroll", () => {
-        if (scrollContainer().scrollTop > showOnPx) {
-            backToTopButton.classList.remove("hidden")
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
         } else {
-            backToTopButton.classList.add("hidden")
+            mybutton.style.display = "none";
         }
-    })
-    const goToTop = () => {
-        document.body.scrollIntoView();
-    };
-    backToTopButton.addEventListener("click", goToTop)
-
-    let navLinks = document.getElementsByClassName('nav-link');
-    for (let i = 0; i < navLinks.length; i++) {
-        navLinks[i].addEventListener("click", function (e) {
-            if (navLinks[i].classList.contains('active')) {
-                navLinks[i].classList.toggle('active')
-            }
-                navLinks[i].classList.toggle('active');
-        })
     }
 })();
