2e68caf4bc8cf4548fca08331f57331beef39e70
Chase Forestello <chase.forestello@gmail.com>
2023-02-07 16:25:49 -0600

    added demo link for movie app

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 (function () {
-    "use strict";
-    let mybutton = document.getElementById("myBtn");
+  "use strict";
+  let mybutton = document.getElementById("myBtn");
 
-    window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () {
+    scrollFunction();
+  };
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
     }
+  }
 })();
